ea1411a3c4ab1231f3fdcc51615daf6951685d51
Vinicius Guedes <viniciusgued@gmail.com>
2018-01-11 17:20:10 -0200

    Refactoring some functions

--- ts/lib/ORM.ts
+++ ts/lib/ORM.ts
@@ -148,9 +148,9 @@ export namespace ORM
                 this.errors[field] = [];
 
             if (message instanceof Error)
-                this.errors[field].push(message.message);
-            else
-                this.errors[field].push(message);
+                return this.errors[field].push(message.message);
+            
+            this.errors[field].push(message);
         }
 
         public getErrors():Array<string>
