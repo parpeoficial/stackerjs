d9d0af46c812c4256f665d6d0a87b96a8b4710a9
Vinicius Guedes <viniciusgued@gmail.com>
2018-05-03 14:37:32 -0300

    refactor(Command): remove repeated functions

--- src/forklift.js
+++ src/forklift.js
@@ -1,97 +1,105 @@
-#! /usr/bin/node
+#!/usr/bin/env node
 
-import { existsSync, readFileSync } from "fs";
+import { existsSync } from "fs";
+import { Command } from "./lib/Command";
+import { resolve } from "path";
 
-const parseRoute = () => 
+
+class Forklift extends Command 
 {
-    let route = [], options = {};
-    process.argv
-        .filter((item, index) => index > 1)
-        .forEach(item => 
-        {
-            if (item.substr(0, 1) === "-") 
-            {
-                let [key, value] = item.split("=");
-                options[key.replace(/-/g, "")] = value || true;
-                return;
-            }
 
-            route.push(item);
-        });
-
-    return [route.join(" "), options];
-};
-
-const loadCommand = name => existsSync(`./commands/${name}.js`) ?
-    require(`./commands/${name}`)[name] : require(`./node_modules/stackerjs/commands/${name}`)[name];
-
-const loadAutoLoad = () => existsSync(`${process.cwd()}/node_modules/.bin/.autoload`) ?
-    JSON.parse(readFileSync(`${process.cwd()}/node_modules/.bin/.autoload`, { encoding: "utf8" })) : null;
-
-const fetchCommand = route => 
-{
-    let autoload = loadAutoLoad(), currentRoute = route.split(" ");
-    if (autoload) 
+    parseRoute() 
     {
-        let command, params = {};
-        Object.keys(autoload.commands).forEach(commandRoute => 
-        {
-            if (command)
-                return;
-
-            let splittedCommandRoute = commandRoute.split(" "),
-                mightBeParams = {};
-            for (let index in splittedCommandRoute) 
+        let route = [], options = {};
+        process.argv
+            .filter((item, index) => index > 1)
+            .forEach(item => 
             {
-                let item = splittedCommandRoute[index];
-                if (item === currentRoute[index])
-                    continue;
-
-                if (item.slice(0, 1) === "{" && item.slice(-1) === "}") 
+                if (item.substr(0, 1) === "-") 
                 {
-                    mightBeParams[item.slice(1, -1)] = currentRoute[index];
-                    continue;
+                    let [key, value] = item.split("=");
+                    options[key.replace(/-/g, "")] = value || true;
+                    return;
                 }
 
-                return;
-            }
+                route.push(item);
+            });
 
-            params = mightBeParams;
-            command = autoload.commands[commandRoute];
-        });
-
-        if (command) 
-        {
-            command.params = params;
-            return command;
-        }
+        return [route.join(" "), options];
     }
 
-    return null;
-};
-
-
-const main = (route, options) => 
-{
-    if (route === "dump")
-        return new (loadCommand("PrepareCommand"))({}, options);
-
-    if (!Command) 
+    fetchCommand(route) 
     {
-        let fetchedCommand = fetchCommand(route);
-        if (fetchedCommand) 
+        let autoload = this.getAutoLoad(), currentRoute = route.split(" ");
+        if (autoload) 
         {
-            let { file, params } = fetchedCommand;
-            return new (loadCommand(file))(params, options);
+            let command, params = {};
+            Object.keys(autoload.commands).forEach(commandRoute => 
+            {
+                if (command)
+                    return;
+
+                let splittedCommandRoute = commandRoute.split(" "),
+                    mightBeParams = {};
+                for (let index in splittedCommandRoute) 
+                {
+                    let item = splittedCommandRoute[index];
+                    if (item === currentRoute[index])
+                        continue;
+
+                    if (item.slice(0, 1) === "{" && item.slice(-1) === "}") 
+                    {
+                        mightBeParams[item.slice(1, -1)] = currentRoute[index];
+                        continue;
+                    }
+
+                    return;
+                }
+
+                params = mightBeParams;
+                command = autoload.commands[commandRoute];
+            });
+
+            if (command) 
+            {
+                command.params = params;
+                return command;
+            }
         }
+
+        return null;
     }
 
-    return null;
-};
+    loadCommand(name) 
+    {
+        if (existsSync(resolve(this.getCommandsPath(), `${name}.js`)))
+            return require(`./commands/${name}`)[name];
 
+        return require(`stackerjs/commands/${name}`)[name];
+    }
 
-let Command = main(...parseRoute());
-if (Command)
-    Command.handle();
-else
-    console.log("Command not found");
+    handle() 
+    {
+        let [route, options] = this.parseRoute(), Command;
+        if (route === "dump")
+            Command = new (this.loadCommand("PrepareCommand"))({}, options);
+
+        if (!Command) 
+        {
+            let fetchedCommand = this.fetchCommand(route);
+            if (fetchedCommand) 
+            {
+                let { file, params } = fetchedCommand;
+                Command = new (this.loadCommand(file))(params, options);
+            }
+        }
+
+        if (Command)
+            Command.handle();
+        else
+            console.log("Command not found");
+    }
+
+}
+
+new Forklift().handle();
